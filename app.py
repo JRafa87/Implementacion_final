66850d4d1a7183183f7a8eee4c191aa0010c431d
JRafa87 <i2321513@continental.edu.pe>
2025-12-09 09:37:40 -0500

    hb

--- app.py
+++ app.py
@@ -493,7 +493,7 @@ if session_is_active:
         "Gestión de Empleados":lambda: render_employee_management_page() , # Función CRUD dedicada
         "Predicción desde Archivo": lambda: render_placeholder_page("Predicción desde Archivo 📁"),
         "Predicción Manual": lambda: render_placeholder_page("Predicción Manual ✏️"),
-        "Reconocimiento": lambda: render_recognition_page()
+        "Reconocimiento": lambda: render_recognition_page(df, risk_df)
     }
     
     # Ejecutar la función de renderizado para la página actual
