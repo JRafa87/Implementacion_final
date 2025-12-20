9e1d4582814d646ccba50c4cf900f000c3604a15
JRafa87 <i2321513@continental.edu.pe>
2025-12-20 15:46:11 -0500

    cgt

--- dashboard_rotacion.py
+++ dashboard_rotacion.py
@@ -60,7 +60,7 @@ def load_data():
 # 2. DASHBOARD
 # ==============================================================================
 
-def render_dashboard_rotacion():
+def ender_rotacion_dashboard():
 
     st.title("📊 Análisis Descriptivo de Rotación de Personal")
     st.caption("Dashboard descriptivo – análisis histórico basado en datos de Supabase")
